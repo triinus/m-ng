ba86c3a525261a26be885ef7a6c6e29189df4f26
triinu.sihver <triinusihver@gmail.com>
2024-05-09 19:12:23 +0300

    vaja teha game over tekst

--- süda.py
+++ süda.py
@@ -1,8 +1,7 @@
 import pygame.sprite
 
 class Süda(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.transform.scale(pygame.image.load("süda.png").convert_alpha(), (10, 10))
-        self.rect = self.image.get_rect(up=(300, 100))
-
+        self.image = pygame.transform.scale(pygame.image.load("süda.png").convert_alpha(), (50, 50))
+        self.rect = self.image.get_rect(center=(x, y))
